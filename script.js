ec9d1c88951b6bf473c95a5cce57d5bf740ac121
”Bill” <gabriel2002curso@gmail.com>
2023-03-26 15:38:04 -0300

    transições

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-var contato = document.getElementById("contato");
+const contato = document.getElementById("contato");
+const conteudoPrincipal = document.getElementsByClassName("conteudoPrincipal");
+const btnProjetos = document.getElementById("btnProjetos");
+const proj = document.getElementsByClassName("proj");
+const projetos = document.getElementsByClassName("projetos");
+const header = document.getElementsByTagName("header");
+const footer = document.getElementsByTagName("footer");
+
+//TEXTO ICONES CONTATO
 contato.addEventListener("mouseover", textIcon);
 contato.addEventListener("mouseout", limpaTextIcon);
 
@@ -20,4 +28,23 @@ function textIcon(e) {
 function limpaTextIcon() {
     nomeIcon.innerText = "";
     contato.style.borderBottomColor = "#fff"
+}
+
+
+// DIV PROJETOS
+btnProjetos.addEventListener("click", aparecerDivProj);
+
+function aparecerDivProj(){
+    proj[0].style.display = "block";
+    setTimeout(()=>{
+        proj[0].style.position = "relative";
+        conteudoPrincipal[0].style.display = "none";
+        projetos[0].style.display = "grid";
+        header[0].style.display = "none";
+        footer[0].style.display = "none";
+        setTimeout(()=>{
+            header[0].style.display = "flex";
+            footer[0].style.display = "grid";
+        }, 20)
+    }, 1000);    
 }
