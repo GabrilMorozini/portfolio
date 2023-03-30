07f43dd3d11ab4f2c10da6f16cbc1bfd1331fe62
”GabrilMorozini” <gabriel2002morozini@gmail.com>
2023-03-29 23:28:37 -0300

    Inserção da logo e projetos

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const conteudo = document.getElementsByClassName("conteudo");
 const conteudoPrincipal = document.getElementsByClassName("conteudoPrincipal");
 const mostrarProj = document.getElementById("mostrarProj");
 const voltarPrincipal = document.getElementById("voltarPrincipal");
+const tituloPagProjetos = document.getElementById('tituloPagProjetos');
+const imgLogo = document.getElementById("logo");
 const proj = document.getElementById("proj");
 const projetos = document.getElementsByClassName("projetos");
+const msgFinalProj = document.getElementById('msgFinalProj');
 const header = document.getElementsByTagName("header");
 const footer = document.getElementsByTagName("footer");
+
+// VAR DE CONTROLE 
+var paginaPrincipalAtiva = true;
 
 //TEXTO ICONES CONTATO
 contato.addEventListener("mouseover", textIcon);
@@ -20,7 +26,7 @@ function textIcon(e) {
         contato.style.borderBottomColor = "#2fff2f";
     } else if (e.target.id == "github") {
         nomeIcon.innerText = "GitHub";
-        contato.style.borderBottomColor = "#753cfc" ;
+        contato.style.borderBottomColor = "#753cfc";
     } else if (e.target.id == "linkedin") {
         nomeIcon.innerText = "Linkedin";
         contato.style.borderBottomColor = "#00a6ff";
@@ -35,43 +41,54 @@ function limpaTextIcon() {
 // DIV PROJETOS
 mostrarProj.addEventListener("click", aparecerDivProj);
 
-function aparecerDivProj(){
+function aparecerDivProj() {
+    paginaPrincipalAtiva = false;
     proj.style.display = "block";
-    setTimeout(()=>{
+    setTimeout(() => {
         conteudoPrincipal[0].style.display = "none";
         proj.style.position = "relative";
         projetos[0].style.display = "grid";
         voltarPrincipal.style.display = "inline-block";
+        tituloPagProjetos.style.display = "block";
+        msgFinalProj.style.display = "block";
         aparecerHeaderFooter(20);
-        proj.classList.remove('proj'); 
-    }, 1500);   
-}
-
-voltarPrincipal.addEventListener("click", desaparecerDivProj);
-
-function desaparecerDivProj(){   
-    let divAnimada = document.createElement('div');
-    let elementoPai = conteudo[0];
-    elementoPai.appendChild(divAnimada);
-    divAnimada.classList.add('posicaoDivAnimada');
-    divAnimada.classList.add('animacaoVoltar');
-    conteudo[0].classList.add('opacidadeAnimada');
-
-    setTimeout(() =>{
-    aparecerHeaderFooter(20);
-    conteudo[0].classList.remove('opacidadeAnimada');
-    proj.classList.add('proj');
-    projetos[0].removeAttribute('style');
-    voltarPrincipal.removeAttribute('style');
-    proj.removeAttribute('style');
-    conteudoPrincipal[0].style.display = "block";
+        proj.classList.remove('proj');
     }, 1500);
 }
 
-function aparecerHeaderFooter(tempo){
+voltarPrincipal.addEventListener("click", desaparecerDivProj);
+imgLogo.addEventListener("click", desaparecerDivProj);
+
+function desaparecerDivProj() {
+    if (paginaPrincipalAtiva == false) {
+        let divAnimada = document.createElement('div');
+        let elementoPai = conteudo[0];
+        elementoPai.appendChild(divAnimada);
+        divAnimada.classList.add('posicaoDivAnimada');
+        divAnimada.classList.add('animacaoVoltar');
+        conteudo[0].classList.add('opacidadeAnimada');
+
+        setTimeout(() => {
+            aparecerHeaderFooter(20);
+            conteudo[0].classList.remove('opacidadeAnimada');
+            proj.classList.add('proj');
+            projetos[0].removeAttribute('style');
+            voltarPrincipal.removeAttribute('style');
+            tituloPagProjetos.removeAttribute('style');
+            proj.removeAttribute('style');
+            msgFinalProj.removeAttribute('style');
+            conteudoPrincipal[0].style.display = "block";
+        }, 1500);
+        paginaPrincipalAtiva = true;
+    }else{
+        return
+    }
+}
+
+function aparecerHeaderFooter(tempo) {
     header[0].style.display = "none";
     footer[0].style.display = "none";
-    setTimeout(()=>{
+    setTimeout(() => {
         header[0].style.display = "flex";
         footer[0].style.display = "grid";
     }, tempo);
